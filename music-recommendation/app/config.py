f567e13e47f717b5836993c013a06968cc59d264
Eg0rG0 <egor.golubev2003@gmail.com>
2025-12-20 03:01:53 +0300

    golubev checkpoint 4 tasks

--- music-recommendation/app/config.py
+++
@@ -1,9 +0,0 @@
-import os
-from app.models.models import AppConfig
-
-
-def parse_config():
-    return AppConfig(
-        host=os.getenv('HOST', '0.0.0.0'),
-        port=os.getenv('PORT', 6969)
-    )
